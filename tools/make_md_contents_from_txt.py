69e934e51040770fc1db2fd66585dc2e7b72021b
TonyPythoneer <tonypythoneer@gmail.com>
2015-08-04 15:18:32 +0800

    Enhance: add make_md_contents_from_txt.py

---
+++ tools/make_md_contents_from_txt.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  @first_date    20150804
+#  @date          20150804
+#  @version       0.1
+"""Fast make contents of README.md
+
+Example:
+
+    How to use this python package. A first method enter command line:
+
+        $ python make_readme_content.py <url>
+
+    A second method enter python interaction mode and input url:
+
+        $ python make_readme_content.py
+        $ >>> Input repo_url:<url>
+"""
+import os
+
+
+def yyy(line):
+    line = line.replace('\n', '')
+    split_list = line.split(' ')
+    sharps = split_list[0].count('#')
+
+    # Process: Erase non-alpha and non-digit except space
+    for word in line:
+        is_space_or_hyphen = word.isspace() or (word is '-')
+        if not is_space_or_hyphen:
+            is_alpha_or_digit = word.isalpha() or word.isdigit()
+            if not is_alpha_or_digit:
+                line = line.replace(word, '')
+
+    return {'sharps': sharps,
+            'title': ' '.join(split_list[1:]),
+            'href': '-'.join(line.split(' ')[1:]).lower()}
+            
+
+def get_contents_from_md(readme_path):
+    contents = []
+    with open(readme_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.startswith('#'):                  
+                contents.append(yyy(line))
+    return contents
+
+
+def make_contents(md_path, contents):
+    with open(md_path, 'r') as f:
+        origin_content = f.readlines()
+
+    with open(md_path, 'w') as f:        
+        for content in contents:
+            print content
+            context = '- [{title}](#{href})'.format(title=content['title'],
+                                                    href=content['href'])
+            print context
+            context = 4 * (content['sharps'] - 1) * ' ' + context + '\n'
+            f.write(context)
+        f.write('\n')
+        f.writelines(origin_content)
+    print 'SUCCESS!!'
+    print 'Contents complete: ' + md_path
+
+
+def input_md_directory():
+    md_list = []
+    directory = raw_input('>>> Input path: ')
+    for each_file in os.listdir(directory):
+        if each_file.endswith(".md"):
+            md_list.append(os.path.join(directory, each_file))
+    return md_list
+
+if __name__ == "__main__":
+    # Action: Enter command line
+    md_list = input_md_directory()
+
+    # Function: Process data
+    for md_path in md_list:
+        contents = get_contents_from_md(md_path)
+        make_contents(md_path, contents)
+        
