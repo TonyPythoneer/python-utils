dd2af0ce9375cc62fd0865fe34090fa647739ee7
TonyPythoneer <tonypythoneer@gmail.com>
2015-08-04 15:51:23 +0800

    Delete: awesome django example has been separate and store in a new repository

--- awesome_example_project_for_django/(basic)(fe)crud_auth_management/app_user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from app_user import views
-
-
-urlpatterns = [
-    url(r'^signup$', views.signup, name='signup'),
-]
--- awesome_example_project_for_django/(basic)(fe)crud_auth_management/website/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-
-
-urlpatterns = [
-    url(r'^$', 'app_user.views.home', name='home'),
-    url(r'^u/', include('app_user.urls', namespace='user')),
-]
